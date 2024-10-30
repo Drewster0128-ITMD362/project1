9faecab87e2d025d49119bf918ee581a9cf3c21c
Drewster0128 <acook21@hawk.iit.edu>
2024-10-30 11:43:23 -0500

    Add focusout on input to mark red if invalid

---
+++ src/script.js
@@ -0,0 +1,39 @@
+//html elements
+const form = document.querySelector('form');
+const formFields = document.querySelectorAll('.form-field');
+
+//console.log(formInputs);
+
+//event functions
+function validateSubmission(event)
+{
+    event.preventDefault()
+    //check criteria
+    formFields.forEach((formField) => hasInput(formField))
+    //valid first name
+    //valid last name
+    //valid email
+}
+
+function hasInput(formField)
+{
+    formInput = formField.querySelector('.form-field__input');
+    if(formInput.value === '' && !formField.classList.contains('form-field--invalid'))
+    {
+        formField.classList.add('form-field--invalid');
+    }
+    else if(formInput.value !== '' && formField.classList.contains('form-field--invalid'))
+    {
+        //add red border
+        formField.classList.remove('form-field--invalid');
+    }
+}
+
+//event listeners
+form.addEventListener('submit', validateSubmission);
+
+formFields.forEach((formField) => {
+    formField.addEventListener('focusout', (event) => {
+        hasInput(formField);
+    }) 
+})
